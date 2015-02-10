4acbe146640a244dbe1a35ed86f86be85087477b
Ryan Sorensen <rcsorensen@gmail.com>
2015-02-10 00:54:42 -0800

    wrap the compiled-grammar to not rely on external parsers. closes #2

--- scripts/compile_parser.js
+++ scripts/compile_parser.js
@@ -5,7 +5,18 @@ var jison = require('jison'),
     grammar = require('../lib/route/grammar.js'),
     parser = new jison.Parser(grammar);
 
+
+var compiledGrammar = parser.generate({moduleType: 'js'});
+
+
 fs.writeFileSync(
   __dirname + '/../lib/route/compiled-grammar.js',
-  parser.generate()
+  [
+    compiledGrammar,
+    "\n\n\nif (typeof require !== 'undefined' && typeof exports !== 'undefined') {",
+    "\nexports.parser = parser;",
+    "\nexports.Parser = parser.Parser;",
+    "\nexports.parse = function () { return parser.parse.apply(parser, arguments); };",
+    "\n}"
+  ].join('')
 );
