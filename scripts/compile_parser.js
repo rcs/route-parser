ce9e713e4846f86673577a9e9057b480682a1787
Erin Dachtler <download333@gmail.com>
2017-04-17 11:20:24 -0700

    Added comments about compile_parser.js changes

--- scripts/compile_parser.js
+++ scripts/compile_parser.js
@@ -10,6 +10,9 @@ var parser = new jison.Parser(grammar);
 // eslint-disable-next-line no-underscore-dangle
 parser.lexer = new Lexer(grammar.lex, null, grammar.terminals_);
 
+// Remove _token_stack label manually until fixed in jison:
+// https://github.com/zaach/jison/issues/351
+// https://github.com/zaach/jison/pull/352
 var compiledGrammar = parser.generate({ moduleType: 'js' }).replace(/_token_stack:\s?/, '');
 
 
